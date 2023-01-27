75f49b4ee8730f98bdd70abcb024d8cb99c019fd
Shaurya Vardhan <shaurya.vardhan.9758@gmail.com>
2023-01-27 20:21:59 +0530

    login updated

--- src/Components/Con_log/Con_log.jsx
+++ src/Components/Con_log/Con_log.jsx
@@ -17,9 +17,9 @@ const Con_log = () => {
 
   return (
     <div id='con_reg_detail'>
-      <input className='Creg' type="text" placeholder='enter consumer id' onChange={(e)=>{setId(e.target.value)}}/>
+      <input className='Creg' type="text" placeholder='Enter consumer id' onChange={(e)=>{setId(e.target.value)}}/>
       <br />
-      <input className='Creg' type="password" placeholder='enter password' onChange={(e)=>{setPass(e.target.value)}}/>
+      <input className='Creg' type="password" placeholder='Enter password' onChange={(e)=>{setPass(e.target.value)}}/>
       <br />
       <button onClick={handlesubmit}>Register</button>
     </div>
--- src/Components/Dis_log/Dis_log.jsx
+++ src/Components/Dis_log/Dis_log.jsx
@@ -17,9 +17,9 @@ const Dis_log = () => {
 
   return (
     <div id='dis_reg_detail'>
-      <input className='Dreg' type="text" placeholder='enter distributor id' onChange={(e)=>{setId(e.target.value)}}/>
+      <input className='Dreg' type="text" placeholder='Enter distributor id' onChange={(e)=>{setId(e.target.value)}}/>
       <br />
-      <input className='Dreg' type="password" placeholder='enter password' onChange={(e)=>{setPass(e.target.value)}}/>
+      <input className='Dreg' type="password" placeholder='Enter password' onChange={(e)=>{setPass(e.target.value)}}/>
       <br />
       <button onClick={handlesubmit}>Register</button>
       
