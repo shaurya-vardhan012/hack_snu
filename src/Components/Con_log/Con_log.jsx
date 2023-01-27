eb3ef698c9f93f6ca30b064caffe5e2abd5531be
Shaurya Vardhan <shaurya.vardhan.9758@gmail.com>
2023-01-27 20:23:27 +0530

    login updated

--- src/Components/Con_log/Con_log.jsx
+++ src/Components/Con_log/Con_log.jsx
@@ -10,16 +10,14 @@ const Con_log = () => {
     }
 
     const [id,setId] = useState("");
-    const [name,setName] = useState("");
     const [pass,setPass] = useState("");
-    const [cpass,setCpass] = useState("");
 
 
   return (
     <div id='con_reg_detail'>
-      <input className='Creg' type="text" placeholder='Enter consumer id' onChange={(e)=>{setId(e.target.value)}}/>
+      <input className='Creg' type="text" placeholder='Enter consumer id' value={id} onChange={(e)=>{setId(e.target.value)}}/>
       <br />
-      <input className='Creg' type="password" placeholder='Enter password' onChange={(e)=>{setPass(e.target.value)}}/>
+      <input className='Creg' type="password" placeholder='Enter password' value={pass} onChange={(e)=>{setPass(e.target.value)}}/>
       <br />
       <button onClick={handlesubmit}>Register</button>
     </div>
