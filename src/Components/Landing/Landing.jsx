d5bffa490f06fe55a9e210361c46abf5fef0b62f
Shaurya Vardhan <shaurya.vardhan.9758@gmail.com>
2023-01-27 17:05:14 +0530

    landing done

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Landing.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser,faFileClipboard, faIndustry } from '@fortawesome/free-solid-svg-icons';
+import { faAmazon } from '@fortawesome/free-brands-svg-icons';
 
 const Landing = () => {
   return (
@@ -9,12 +12,15 @@ const Landing = () => {
         </div>
         <div className="landing-main">
             <div className="landing-card">
+                <FontAwesomeIcon icon={faIndustry} style={{fontSize:"90px",margin:"30px"}}/>
                 <button>Manufacture</button>
             </div>
             <div className="landing-card">
+                <FontAwesomeIcon icon={faFileClipboard} style={{fontSize:"90px",margin:"30px"}}/>
                 <button>Distributor</button>
             </div>
             <div className="landing-card">
+                <FontAwesomeIcon icon={faUser} style={{fontSize:"90px",margin:"30px"}}/>
                 <button>Customer</button>
             </div>
         </div>
